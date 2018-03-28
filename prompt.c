3e8d5bec1fb10b9c02cc54c975fd2693b001f75b
Jeremy Combe <jecombe@z1r9p1.le-101.fr>
2018-03-28 16:56:02 +0200

    ajrd

--- prompt.c
+++ prompt.c
@@ -6,12 +6,53 @@
 /*   By: jecombe <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/27 17:53:36 by jecombe      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/28 15:46:02 by jecombe     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/28 16:55:48 by jecombe     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int		ft_check_space(char *buff)
+{
+	int i;
+	i = 0;
+	while (buff[i] == ' ' || buff[i] == '\n')
+		i++;
+	if ((buff[i] >= 'a' && buff[i] <= 'z') || (buff[i] >= 'A' && buff[i] <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+char		*ft_epure(char *str)
+{
+	int i;
+	int j;
+
+	if (!str)
+		return(0);
+	i = -1;
+	j = 0;
+	while (str[++i])
+	{
+		if (str[i] != ' ' && str[i] != '\t')
+		{
+			str[j] = str[i];
+			j++;
+			if (str[i + 1] ==  ' ' || str[i + 1] == '\t')
+			{
+				printf("ok\n");
+				str[j] = ' ';
+				j++;
+			}
+		}
+	}
+	str[j] = '\0';
+	if (str[j - 1] == ' ')
+		str[j - 1] = '\0';
+	return (str);
+}
 
 void		aff_prompt(char **argv, t_minishell *shell)
 {
@@ -33,7 +74,12 @@ void		aff_prompt(char **argv, t_minishell *shell)
 			exit(0);
 		}
 		printf("-----> %s\n", cmd);
+		if (ft_check_space(buff) == 0)
+			return;
+		cmd = ft_epure(buff);
+		printf("***********> %s\n", cmd);
 		/*if (ft_builtin(cmd, shell) == 1)
+		printf("-----> %s\n", cmd);
 		return;*/
 	}
 
