780116036cb649382767b1a62fb99807c34fdf42
Jeremy Combe <jecombe@z1r9p1.le-101.fr>
2018-04-22 17:29:46 +0200

    NORME

--- libft/ft_match_before_char.c
+++ libft/ft_match_before_char.c
@@ -6,26 +6,29 @@
 /*   By: jecombe <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/22 13:31:55 by jecombe      #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/22 16:22:38 by jecombe     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/22 17:29:34 by jecombe     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int		ft_match_before_char(char *cmd, char c, char *env)
 {
 	int i;
 	int y;
+
 	i = 0;
 	y = 0;
-
 	while (env[i + 1] != c && cmd[y] == env[i])
 	{
-			i++;
-			y++;
+		i++;
+		y++;
 	}
 	if (cmd[y] != env[i])
+		return (0);
+	if (cmd[y + 1] != '\0')
 		return (0);
 	return(1);
 }
