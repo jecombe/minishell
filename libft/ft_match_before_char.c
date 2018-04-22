e1a8f8c860715f4cc2d1e32fca8aeb5208363594
Jeremy Combe <jecombe@z1r9p1.le-101.fr>
2018-04-22 16:23:00 +0200

    NORME

--- libft/ft_match_before_char.c
+++ libft/ft_match_before_char.c
@@ -6,7 +6,7 @@
 /*   By: jecombe <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/22 13:31:55 by jecombe      #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/22 15:08:14 by jecombe     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/22 16:22:38 by jecombe     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -25,7 +25,7 @@ int		ft_match_before_char(char *cmd, char c, char *env)
 			i++;
 			y++;
 	}
-	if (cmd[y] != env[i] || cmd[y] != '\0')
+	if (cmd[y] != env[i])
 		return (0);
 	return(1);
 }
